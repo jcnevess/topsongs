d09f5254d8efd5a58b14d04a13112b1983eae2a7
Júlio César Neves <juliocnevess@gmail.com>
2022-04-30 10:08:06 -0300

    Add access to genius API using bearer token

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 # coding: utf-8
 
+import uuid
+import os
 import requests
 
-req = requests.get('https://api.genius.com/artists/16775/songs?sort=popularity&per_page=10')
-print(req.json())
+from oauthlib.oauth2 import WebApplicationClient
+
+CLIENT_ID = os.getenv('CLIENT_ID')
+ACCESS_TOKEN = os.getenv('ACCESS_TOKEN')
+
+client = WebApplicationClient(CLIENT_ID)
+
+header = {
+    'Authorization': 'Bearer {}'.format(ACCESS_TOKEN)
+}
+
+res = requests.get('https://api.genius.com/artists/16775/songs?sort=popularity&per_page=10',
+    headers = header)
+
+print(res.json())
+
+transaction_id = uuid.uuid4()
